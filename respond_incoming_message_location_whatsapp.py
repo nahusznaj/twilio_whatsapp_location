d74681165cb8fb12456b64a27e9f6776ef0351ff
nahusznaj <nsznaj@gmail.com>
2019-12-10 21:05:45 +0000

    first commit
    sample app showing how to manage the params that Twilio will POST to the webhook including location

---
+++ respond_incoming_message_location_whatsapp.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+    
+    
+    if "Address" in request.values and "Latitude" in request.values:
+        latitude = request.values.get('Latitude', None) 
+        address = request.values.get('Address', None) 
+        resp.message("The Robots are coming! Head for the hills!, your location is " + latitude + " and address is " + address)
+        
+    elif "Latitude" in request.values:
+        latitude = request.values.get('Latitude', None) 
+        resp.message("The Robots are coming! Head for the hills!, your location is " + latitude + " and no address!")
+    else:
+        resp.message("The Robots are coming! Head for the hills! You didn't send any address or latiture")
+
+    return str(resp)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
